d550ce1da18cf77c4b64f63c235f9f9905c2852f
Tito Bouzout <tito.bouzout@gmail.com>
2014-05-27 20:48:10 -0300

    Fixes #1

--- PreventFakeClones.py
+++ PreventFakeClones.py
@@ -8,8 +8,6 @@ def normalize(path):
 class prevent_fake_clones_listener(sublime_plugin.EventListener):
 
 	def on_load(self, view):
-
-		# Prevent Fake Clones
 		if view.file_name():
 			path = normalize(view.file_name())
 			window = view.window()
@@ -22,14 +20,28 @@ class prevent_fake_clones_listener(sublime_plugin.EventListener):
 						_view.buffer_id() != view.buffer_id() and # if the buffer is not the same (if is not a real clone)
 						path == normalize(_view.file_name()) # if the path of the file matches exactly
 					) :
-						# close it
-						window.focus_view(view)
-						window.run_command('close')
+						# If the other view is not "dirty", then close it, and just let the user open the view that is opening.
+						if not _view.is_dirty():
+							self.focus_view(_view)
+							_window.run_command('close')
+							self.focus_view(view)
+						else:
+							# If the other view is "dirty" and in the same window, just focus it.
+							if window == _window:
+								self.focus_view(view)
+								window.run_command('close')
+								self.focus_view(_view)
+							# If the other view is "dirty" and is in another window. Display the popup/statusbar message
+							else:
+								# close it
+								window.run_command('close')
 
-						# focus the first instance
-						_window.focus_view(_view)
-						_window.run_command('focus_neighboring_group')
-						_window.focus_view(_view)
-						_window.run_command('clone_file')
-						sublime.error_message('Preventing opening an already opened file (aka "fake clone")\nFocusing already opened file....\n\nUse: "File -> New File into View" which will open a real clone.')
+								self.focus_view(_view)
+								sublime.status_message('Preventing opening an already opened file (aka "fake clone") Focusing already opened file...  Use: "File -> New File into View" which will open a real clone.')
 						return
+
+	def focus_view(self, view):
+		window = view.window()
+		window.focus_view(view)
+		window.run_command('focus_neighboring_group')
+		window.focus_view(view)
